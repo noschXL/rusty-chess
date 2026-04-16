510c3ad25f5221296cc94309002885804d9cfe53
noschXL <m.nosch@proton.me>
2026-04-16 10:41:18 +0200

    added the first constants and added a filestructure

---
+++ src/engine/constants.rs
@@ -0,0 +1,10 @@
+const WHITE: u32 = 0b0;
+const BLACK: u32 = 0b1;
+
+const EMPTY: u32 = 0b0000;
+const PAWN: u32 = 0b0010;
+const KNIGHT: u32 = 0b0100;
+const BISHOP: u32 = 0b0110;
+const ROOK: u32 = 0b1000;
+const QUEEN: u32 = 0b1010;
+const KING: u32 = 0b1100;
